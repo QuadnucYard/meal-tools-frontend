3f2413d6b866ae644793b4cbc898d48919185592
QuadnucYard <2380433991@qq.com>
2024-03-25 11:59:59 +0800

    Fix: food price in form

--- src/views/components/CreateFoodDialog.vue
+++ src/views/components/CreateFoodDialog.vue
@@ -5,41 +5,49 @@
         <div class="text-h6">创建食物</div>
       </q-card-section>
       <q-card-section class="q-gutter-md">
-        <q-input v-model="food.name" label="名称" filled />
-        <q-select
-          v-model="food.aliases"
-          label="别名"
-          filled
-          use-input
-          use-chips
-          multiple
-          hide-dropdown-icon
-          input-debounce="0"
-          new-value-mode="add"
-        />
-        <q-input v-model="food.desc" label="描述" type="textarea" filled />
-        <q-item>
-          <q-item-section avatar> 标签 </q-item-section>
-          <q-item-section>
-            <TagSelect v-model="food.tag_ids" />
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section avatar> 价格 </q-item-section>
-          <q-item-section>
-            <q-slider
-              v-model="food.price"
-              markers
-              marker-labels
-              label
-              label-always
-              :min="0"
-              :max="5"
-              :step="0.5"
-              snap
-            />
-          </q-item-section>
-        </q-item>
+        <q-form @submit="onConfirm">
+          <q-input
+            v-model="food.name"
+            label="名称"
+            filled
+            :rules="[(val) => (val != '' && !Number.isNaN(Number(val))) || '请输入名称']"
+          />
+          <q-select
+            v-model="food.aliases"
+            label="别名"
+            filled
+            use-input
+            use-chips
+            multiple
+            hide-dropdown-icon
+            input-debounce="0"
+            new-value-mode="add"
+          />
+          <q-input v-model="food.desc" label="描述" type="textarea" filled />
+          <q-item>
+            <q-item-section avatar> 标签 </q-item-section>
+            <q-item-section>
+              <TagSelect v-model="food.tag_ids" />
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section avatar> 价格 </q-item-section>
+            <q-item-section>
+              <q-slider
+                v-model="food.price"
+                markers
+                label
+                label-always
+                :label-value="(food.price / 10).toFixed(1)"
+                :marker-labels="(x: number) => (x / 10).toFixed(1)"
+                :min="0"
+                :max="50"
+                :step="5"
+                snap
+              />
+            </q-item-section>
+          </q-item>
+        </q-form>
       </q-card-section>
 
       <q-card-actions align="right">
@@ -62,7 +70,7 @@ const visible = ref(false);
 const food = reactive<FoodCreate>({
   name: "",
   aliases: [],
-  price: 2,
+  price: 20,
   desc: "",
   images: [],
   tag_ids: [] as int[],
@@ -79,7 +87,6 @@ const show = () => {
 
 const onConfirm = async () => {
   try {
-    food.price = food.price * 10;
     const result = await foodStore.create(food);
     Message.success("成功创建食物");
     promiseStatus?.resolve(result);
