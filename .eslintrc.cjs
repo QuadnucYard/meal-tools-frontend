8802881c83190a8c785e5721547a55a25c87aa90
QuadnucYard <2380433991@qq.com>
2024-04-02 09:57:36 +0800

    Migrate to bun

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,19 @@
 /* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
+    "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    // "@vue/eslint-config-typescript",
-    // "@vue/eslint-config-prettier/skip-formatting",
-    "./.eslintrc-auto-import.json",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
-  parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
   },
-  // plugins: ["@typescript-eslint", "vue"],
   rules: {
-    indent: ["warn", 2],
-    quotes: ["warn", "double"],
-    semi: ["warn", "always"],
     "vue/multi-word-component-names": "off",
     "@typescript-eslint/no-explicit-any": "off",
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.vue"],
-      rules: {
-        "no-undef": "off",
-      },
-    },
-  ],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import * as path from "path";
+import { URL, fileURLToPath } from "node:url";
 import AutoImport from "unplugin-auto-import/vite";
 import { QuasarResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
@@ -8,16 +8,13 @@ import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import circleDependency from "vite-plugin-circular-dependency";
 import viteCompression from "vite-plugin-compression";
 
-const pathSrc = path.resolve(__dirname, "src");
-
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) =>
   defineConfig({
     base: loadEnv(mode, process.cwd()).VITE_BASE_PATH,
     resolve: {
       alias: {
-        "@": path.join(__dirname, "./src"),
-        "~/": `${pathSrc}/`,
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
       extensions: [".js", ".json", ".ts"],
     },
